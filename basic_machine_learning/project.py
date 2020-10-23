42eea78044de20da3d70fdb3a415402245e75058
dwiknrd <dwiknrd@gmail.com>
2020-10-23 12:23:06 +0700

    correlation location

--- basic_machine_learning/project.py
+++ basic_machine_learning/project.py
@@ -5,3 +5,11 @@ print('\nLima data teratas:\n', dataset.head())
 print('\nInformasi dataset:')
 print(dataset.info())
 print('\nStatistik deskriptif:\n', dataset.describe())
+
+dataset_corr = dataset.corr()
+print('Korelasi dataset:\n', dataset.corr())
+print('Distribusi Label (Revenue):\n', dataset['Revenue'].value_counts())
+# Tugas praktek
+print('\nKorelasi BounceRates-ExitRates:', dataset_corr.loc['BounceRates', 'ExitRates'])
+print('\nKorelasi Revenue-PageValues:', dataset_corr.loc['Revenue', 'PageValues'])
+print('\nKorelasi TrafficType-Weekend:', dataset_corr.loc['TrafficType', 'Weekend'])
