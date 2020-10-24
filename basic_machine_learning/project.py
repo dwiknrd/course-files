d875a400458eff7fbdf6f9bed35ba8bf501e9a94
dwiknrd <dwiknrd@gmail.com>
2020-10-24 08:47:33 +0700

    treatment missing value

--- basic_machine_learning/project.py
+++ basic_machine_learning/project.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 dataset = pd.read_csv('online_raw.csv')
 print('Shape dataset:', dataset.shape)
 print('\nLima data teratas:\n', dataset.head())
@@ -13,3 +15,37 @@ print('Distribusi Label (Revenue):\n', dataset['Revenue'].value_counts())
 print('\nKorelasi BounceRates-ExitRates:', dataset_corr.loc['BounceRates', 'ExitRates'])
 print('\nKorelasi Revenue-PageValues:', dataset_corr.loc['Revenue', 'PageValues'])
 print('\nKorelasi TrafficType-Weekend:', dataset_corr.loc['TrafficType', 'Weekend'])
+
+# checking the Distribution of customers on Revenue
+plt.rcParams['figure.figsize'] = (12,5)
+plt.subplot(1, 2, 1)
+sns.countplot(dataset['Revenue'], palette = 'pastel')
+plt.title('Buy or Not', fontsize = 20)
+plt.xlabel('Revenue or not', fontsize = 14)
+plt.ylabel('count', fontsize = 14)
+# checking the Distribution of customers on Weekend
+plt.subplot(1, 2, 2)
+sns.countplot(dataset['Weekend'], palette = 'inferno')
+plt.title('Purchase on Weekends', fontsize = 20)
+plt.xlabel('Weekend or not', fontsize = 14)
+plt.ylabel('count', fontsize = 14)
+plt.show()
+
+# visualizing the distribution of customers around the Region
+plt.hist(dataset['Region'], color = 'lightblue')
+plt.title('Distribution of Customers', fontsize = 20)
+plt.xlabel('Region Codes', fontsize = 14)
+plt.ylabel('Count Users', fontsize = 14)
+plt.show()
+
+#checking missing value for each feature  
+print('Checking missing value for each feature:')
+print(dataset.isnull().sum())
+#Counting total missing value
+print('\nCounting total missing value:')
+print(dataset.isnull().sum().sum())
+
+#Drop rows with missing value   
+dataset_clean = dataset.dropna()  
+print('Ukuran dataset_clean:', dataset_clean.isnull().shape) 
+
